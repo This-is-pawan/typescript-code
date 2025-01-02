a2cc5232d43df7e08692378af275e9cfa9c55646
pawan <pawanjalandhara@gmail.com>
2025-01-02 17:49:55 +0530

    typescript

---
+++ src/tutorial.ts
@@ -0,0 +1,61 @@
+// const names:string[]=['john','jam',
+// 'jak','jali'
+// ]
+// function textindcludes(name:string):boolean {
+//  return names.includes(name)
+// }
+// const nameTocheck=['john','jam',
+//  'jak','jkk'
+//  ]
+// if (textindcludes(nameTocheck[3])) {
+
+//  console.log(`${[nameTocheck]} is the list`);
+ 
+// } else {
+//  console.log(`${nameTocheck} is not the list`);
+// }
+
+
+
+// function calculatePrice(price: number, discount?: number): number {
+//  return price - (discount || 0);
+// }
+
+// let priceAfterDiscount = calculatePrice(100, 20);
+// console.log(priceAfterDiscount)
+
+// function calculateScore(initialScore: number, penaltyPoints: number = 0): number {
+//  return initialScore - penaltyPoints;
+// }
+
+// let scoreAfterPenalty1 = calculateScore(300,0 )
+// let scoreAfterPenalty2 = calculateScore(300); 
+// console.log(scoreAfterPenalty1)
+//  console.log(scoreAfterPenalty2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
