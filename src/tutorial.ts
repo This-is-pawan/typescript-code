c25952bd4efe2c459946aedf524bf98788146b09
pawan <pawanjalandhara@gmail.com>
2025-01-02 20:07:48 +0530

    adding functionally

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -34,8 +34,26 @@
 //  console.log(scoreAfterPenalty2)
 
 
+// normal 
+function stingArray(length:number,value:string):string [] {
+ let result:string[];
+result=Array(length).fill(value)
+ return result
+}
+console.log( stingArray(6,'hello'));
 
+// generics
 
+function StingArray<T>(length:number,value:T):Array<T>{
+ let result:T[]=[];
+  result=Array(length).fill(value)
+ return result
+}
+let arraySting1=StingArray<string>(10,'hello');
+let arraySting2=StingArray<string>(5,'hello world')
+console.log(arraySting2,arraySting1);
+
+console.log( StingArray (6,'hello'));
 
 
 
