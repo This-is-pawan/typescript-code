58cfe3ae53d87ef86cc97f15e5cc9feea521c8ce
Pawan <154042595+This-is-pawan@users.noreply.github.com>
2025-09-13 10:38:08 +0530

    Update Practice-tea-code.ts

--- Practice-tea-code.ts
+++ Practice-tea-code.ts
@@ -1,3 +1,9 @@
 console.log("hary");
 let gretting:string='hello'
 console.log(gretting);
+// with function 
+function Practice(name:string,age:number) {
+ console.log(name,age);
+ 
+}
+Practice('deepak',20) 
