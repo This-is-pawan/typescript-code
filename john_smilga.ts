440ebbc5aa1f289006a410461bd9314cb7a4f3fa
Pawan <154042595+This-is-pawan@users.noreply.github.com>
2025-09-17 13:42:42 +0530

    Update john_smilga.ts

--- john_smilga.ts
+++ john_smilga.ts
@@ -1,3 +1,19 @@
 // /@############################# create app with vite typescript
 // npm create vite@latest typescript -- --template vanilla-ts
 // /@#############################
+interface someValue{
+ name:string,
+ id:number,
+}
+const someObj:someValue={
+ name:'random',
+ id:123,
+}
+
+function practice(){
+console.log(someObj);
+
+ console.log('typescript is cal');
+}
+
+// #########
