d7c6a56df173abadc3cd98c1f6b28f4ee33d5a2c
Pawan <154042595+This-is-pawan@users.noreply.github.com>
2025-09-18 10:31:45 +0530

    Update john_smilga.ts

--- john_smilga.ts
+++ john_smilga.ts
@@ -17,3 +17,12 @@ console.log(someObj);
 }
 
 // #########
+// array is ts 
+let Strings:string[]=['this is code of ts'];
+console.log(Strings);
+let Numbers:number[]=[12,4,2,53,6,4]
+console.log(Numbers);
+
+let Both:(number|string)[]=[23,'the',43]
+console.log(Both);
+// ####
