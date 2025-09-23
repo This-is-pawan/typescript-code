5723f55127bb11bfcc8043403f8c25e38e00b80b
Pawan <154042595+This-is-pawan@users.noreply.github.com>
2025-09-23 13:16:29 +0530

    Update john_smilga.ts

--- john_smilga.ts
+++ john_smilga.ts
@@ -373,8 +373,24 @@ const storeNumbers:StoreData<number>={
 const randomStuff:StoreData={
   data:['random',234];
 }
+// ################################################################
+const {data}=axios.get(someUrl)
+axios.get<{name:string}[]>(someUrl);
+export class Axios{
+  get<T=any,R=AxiosResponse<T>,D=any>(url:string,config?:AxiosRequestConfig<D>
 
+  ):Promise<R>
 
+}
+export interface AxiosResponse<T=any,D=any>{
+  data:T;
+  status:number;
+  statusText:string;
+  headers:RawAxiosResponseHeaders| AxiosResponseHeaders;
+  config:InternalAxiosRequestConfig<D>;
+  request?:any;
+}
+// ###########################################################################
 
 
 
