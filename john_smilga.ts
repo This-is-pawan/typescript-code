86f977eca3ac211f3b3a4f7929ea657963f2ae55
Pawan <154042595+This-is-pawan@users.noreply.github.com>
2025-09-23 09:13:06 +0530

    Update john_smilga.ts

--- john_smilga.ts
+++ john_smilga.ts
@@ -224,6 +224,39 @@ console.log(user);
 
 // ######################################################################
 
+// type Dog  ={type:'dog',name:string,bark:()=>void}
+// type Cat={type:'cat',name:string,meow:()=>void}
+// type Animal =Dog|Cat
+// // console.log(Animation);
+// function makeSound(animal:Animal) {
+//   if (animal.type==='dog') {
+//     animal.bark()
+//   } else {
+//     animal.meow()
+//   }
+// }
+// function makeSound(animal:Animal) {
+//   if ('bark' in animal) {
+//     animal.bark()
+//   } else {
+//     animal.meow()
+//   }
+// }
+
+
+// ##############################################################################
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
